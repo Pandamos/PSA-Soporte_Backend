ecd4af2dedd4c15b01d4113d5de08f362679823c
lauradefeo <ldefeo@fi.uba.ar>
2022-07-03 15:22:55 -0300

    "TicketTable"

---
+++ src/main/java/com/example/demo/ticket/TicketTable.java
@@ -0,0 +1,105 @@
+package com.example.demo.ticket;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.text.DateFormat;
+
+@Entity
+@Table(name = "tickets")
+public class TicketTable implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "CUIT_cliente",length = 50)
+    private String CUIT;
+
+    @Column(name = "fecha_creacion",length = 50)
+    private DateFormat fechaDeCreacion;
+
+    @Column(name = "fecha_finalizacion",length = 50)
+    private DateFormat fechaDeFinalizacion;
+
+    @Column(name = "descripcion",length = 50)
+    private String descripcion;
+
+    @Column(name = "severidad",length = 50)
+    private Integer severidad;
+
+    @Column(name = "estado",length = 50)
+    private String estado;
+
+    //public TicketTable() {
+   // }
+
+    public TicketTable(Integer id, String descripcion, Integer severidad,DateFormat fechaDeCreacion,DateFormat fechaDeFinalizacion,String CUIT,String estado){
+        super();
+        this.id = id;
+        this.descripcion = descripcion;
+        this.severidad = severidad;
+        this.estado = estado;
+        this.fechaDeCreacion = fechaDeCreacion;
+        this.fechaDeFinalizacion = fechaDeFinalizacion;
+        this.CUIT = CUIT;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getSeveridad() {
+        return severidad;
+    }
+
+    public void setSeveridad(Integer severidad) {
+        this.severidad = severidad;
+    }
+
+    public DateFormat getFechaDeFinalizacion() {
+        return fechaDeFinalizacion;
+    }
+
+    public void setFechaDeFinalizacion(DateFormat fechaDeFinalizacion) {
+        this.fechaDeFinalizacion = fechaDeFinalizacion;
+    }
+
+    public DateFormat getFechaDeCreacion() {
+        return fechaDeCreacion;
+    }
+
+    public void setFechaDeCreacion(DateFormat fechaDeCreacion) {
+        this.fechaDeCreacion = fechaDeCreacion;
+    }
+
+    public String getCUIT() {
+        return CUIT;
+    }
+
+    public void setCUIT(String CUIT) {
+        this.CUIT = CUIT;
+    }
+
+}
