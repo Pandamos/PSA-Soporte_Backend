0e202be5091a1c28e738022509e010890a39bd00
abrildiazmiguez <adiazm@fi.uba.ar>
2022-07-10 23:26:27 -0300

    Correcciones en dependencias de maven

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,28 @@
 package com.example.demo;
 
-import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSwagger2
 @SpringBootApplication
-@Configuration
+@Controller
 public class DemoApplication {
 
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+				.apis(RequestHandlerSelectors.any())
+				.paths(Predicates.not(PathSelectors.regex("/error")))
+				.paths(Predicates.not(PathSelectors.regex("/")))
 				.paths(PathSelectors.any())
 				.build();
 	}
